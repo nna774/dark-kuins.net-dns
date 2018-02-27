746b4f84e349f355e61fe91dbd4b583b0116e675
Kugayama Nana <nonamea774@gmail.com>
2018-02-27 09:53:37 +0900

    add kizuna-6

--- records.tf
+++ records.tf
@@ -1,4 +1,4 @@
-resource "cloudflare_record" "test" {
+resource "cloudflare_record" "kizuna" {
   domain = "${var.cloudflare_domain}"
   name   = "kizuna.kitashirakawa.${var.cloudflare_domain}"
   value  = "192.50.220.190"
@@ -6,4 +6,25 @@ resource "cloudflare_record" "test" {
   proxied = false
 #  ttl    = 3600
 }
-
+resource "cloudflare_record" "kizuna-6" {
+  domain = "${var.cloudflare_domain}"
+  name   = "kizuna.kitashirakawa.${var.cloudflare_domain}"
+  value  = "2001:df0:8500:a700::1"
+  type   = "AAAA"
+  proxied = false
+}
+resource "cloudflare_record" "kizuna-force4" {
+  domain = "${var.cloudflare_domain}"
+  name   = "v4.kizuna.kitashirakawa.${var.cloudflare_domain}"
+  value  = "192.50.220.190"
+  type   = "A"
+  proxied = false
+#  ttl    = 3600
+}
+resource "cloudflare_record" "kizuna-force6" {
+  domain = "${var.cloudflare_domain}"
+  name   = "v6.kizuna.kitashirakawa.${var.cloudflare_domain}"
+  value  = "2001:df0:8500:a700::1"
+  type   = "AAAA"
+  proxied = false
+}

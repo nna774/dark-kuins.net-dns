4cc5901c66f3c72d1a1479a0746d65dfa99104d1
Kugayama Nana <nonamea774@gmail.com>
2018-03-28 18:01:49 +0900

    add @ record to see google app engine

--- records.tf
+++ records.tf
@@ -42,3 +42,26 @@ resource "cloudflare_record" "google-site-verification" {
   type   = "TXT"
   ttl    = 86400
 }
+
+variable "gae_ips" {
+  default = ["216.239.32.21", "216.239.34.21", "216.239.36.21", "216.239.38.21"]
+}
+variable "gae_ipv6s" {
+  default = ["2001:4860:4802:32::15", "2001:4860:4802:34::15", "2001:4860:4802:36::15", "2001:4860:4802:38::15"]
+}
+resource "cloudflare_record" "at" {
+  count  = "${length(var.gae_ips)}"
+  domain = "${var.cloudflare_domain}"
+  name   = "${var.cloudflare_domain}"
+  value  = "${element(var.gae_ips, count.index)}"
+  type   = "A"
+  proxied = false
+}
+resource "cloudflare_record" "at-aaaa" {
+  count  = "${length(var.gae_ipv6s)}"
+  domain = "${var.cloudflare_domain}"
+  name   = "${var.cloudflare_domain}"
+  value  = "${element(var.gae_ipv6s, count.index)}"
+  type   = "AAAA"
+  proxied = false
+}
